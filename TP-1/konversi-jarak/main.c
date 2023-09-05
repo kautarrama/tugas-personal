9e42e51c74bf68a21d904370f937a474f7a29fc9
Khautal Hilmi Ramadhan <khautalrama@gmail.com>
2023-09-06 02:05:49 +0700

    fix: adjustment console first assignment

--- TP-1/konversi-jarak/main.c
+++ TP-1/konversi-jarak/main.c
@@ -10,12 +10,17 @@ float metersToCentimeters(float value)
     return value * 100;
 }
 
+float kilometersToCentimeters(float value)
+{
+    return value * 100000;
+}
+
 int main() {
     float kilometers, meters, centimeters;
 
-    printf("=================================\n");
-    printf("PROGRAM KONVERSI JARAK\n");
-    printf("=================================\n");
+    printf("============================================\n");
+    printf("\tPROGRAM KONVERSI JARAK\n");
+    printf("============================================\n");
 
     printf("Masukkan jarak(km): ");
     scanf("%f", &kilometers);
@@ -23,9 +28,14 @@ int main() {
     meters = kilometersToMeters(kilometers);
     centimeters = metersToCentimeters(meters);
 
-    printf("\nHasil konversi:\n");
-    printf("Jarak dalam meter: %.2f m\n", meters);
-    printf("Jarak dalam sentimeter: %.2f cm\n", centimeters);
+    printf("\n\tHasil konversi\n");
+    printf("\t%.2f Kilometer(km)\n", kilometers);
+    printf("Meter(m): %.2fm\n", meters);
+    printf("Sentimeter(cm): %.2fcm\n\n", centimeters);
+
+    printf("=========================================\n");
+    printf("\tTERIMA KASIH\n");
+    printf("=========================================\n");
 
     return 0;
 }
